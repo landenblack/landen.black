5a390192c48e8e99a3d817f35a0c03dac8cc55e3
Landen Black <landen.wesley@gmail.com>
2018-11-20 20:20:05 -0600

    Update TestGame.ts

--- game/TestGame.ts
+++ game/TestGame.ts
@@ -25,7 +25,7 @@ class TestGame extends GameBase
 
         this.Context.clear(this.Context.COLOR_BUFFER_BIT);
         //this.Batch.QueueDraw(this.Cocoa, new Rectangle(100, 100, 300, 99)); // x y w h 
-        this.Batch.QueueDraw(this.Cocoa, new Rectangle(100, 100, 60, 99), new Rectangle(0, 0, 20, 33)); // x y w h 
+        this.Batch.QueueDraw(this.Cocoa, new Rectangle(100, 100, 66, 99), new Rectangle(0, 0, 22, 33)); // x y w h 
         this.Batch.ExecuteDraws();
     }
 }
