42fecf85a408b2db51a1409a8ab9d53ca9027d38
Landen Black <landen.wesley@gmail.com>
2018-11-20 20:27:56 -0600

    Update TestGame.ts

--- game/TestGame.ts
+++ game/TestGame.ts
@@ -11,7 +11,7 @@ class TestGame extends GameBase
 
     public Initialize() : void
     {
-        this.Context.clearColor(0.30, 0.18, 0.61, 1.0);
+        this.Context.clearColor(30.0, 0.18, 0.61, 1.0);
         this.Context.enable(this.Context.BLEND);
         this.Context.blendFunc(this.Context.SRC_ALPHA, this.Context.ONE_MINUS_SRC_ALPHA);
 
