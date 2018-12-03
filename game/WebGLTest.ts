29997264021381846892c6762f6a167b2cb920d9
Landen Black <landen.wesley@gmail.com>
2018-12-02 20:12:21 -0600

    Update WebGLTest.ts

--- game/WebGLTest.ts
+++ game/WebGLTest.ts
@@ -6,8 +6,8 @@ let LastTime : number;
 function Main() 
 {
     var Canvas = document.createElement("canvas");
-    Canvas.setAttribute("width", "1800");
-    Canvas.setAttribute("height", "1080");
+    Canvas.setAttribute("width", "1000");
+    Canvas.setAttribute("height", "1000");
     document.body.appendChild(Canvas);
 
     const Context = Canvas.getContext("webgl2");
