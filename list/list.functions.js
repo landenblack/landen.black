362fb5f72bbb13cdac89c22265a4d461f789f34f
Audrey <43182190+aylwg@users.noreply.github.com>
2018-12-15 22:24:51 -0600

    Update list.functions.js

--- list/list.functions.js
+++ list/list.functions.js
@@ -3,8 +3,9 @@ function hello() {
     $.ajax({
         type: "POST",
         url: "/list/server/",
+        data: JSON.stringify({t: "z"}),
         dataType: "json",
-        data: JSON.stringify({t: "z"})
+        contentType: 'application/json; charset=utf-8'
     }).done(function (data) {
         console.log(data.a);
     });
