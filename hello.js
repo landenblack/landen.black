883f2f55def390326b3c86a321688a0ff81806b5
Audrey <43182190+aylwg@users.noreply.github.com>
2018-12-15 22:18:29 -0600

    Update hello.js

--- hello.js
+++ hello.js
@@ -12,6 +12,7 @@ app.use("/tetris", express.static('tetris'));
 app.use("/tetris/local", express.static('tetris/local'));
 app.use("/public", express.static('public'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 
 app.post("/git/", function (req, res) {
