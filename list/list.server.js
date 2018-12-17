0442eca425de519e1b8e8439184694f384a1a10e
Audrey <43182190+aylwg@users.noreply.github.com>
2018-12-16 20:28:12 -0600

    test

--- list/list.server.js
+++ list/list.server.js
@@ -6,6 +6,7 @@ module.exports = function() {
     this.getLists = function(user) {
         console.log(user);
         var lists = this.jsonFile("./list/lists.json");
+        console.log(lists);
         user_lists = lists.filter(list => list.user === user);
         console.log(user_lists);
         return user_lists;
