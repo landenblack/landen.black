04864d60ec294e75f6b80080fe2faa34184288c4
Audrey <43182190+aylwg@users.noreply.github.com>
2018-12-16 20:08:51 -0600

    Update list.client.js

--- list/list.client.js
+++ list/list.client.js
@@ -7,5 +7,21 @@ function getBooks() {
         dataType: "json"
     }).done(function (data) {
         console.log(data);
+        for (let book in data) {
+            console.log(book);
+        }
     });
+}
+
+function bookHTML(book) {
+    hmtl = `
+    <li>
+      <div class="book" data-id="${book._id}">
+          <div class="image"><img src="${book.picture}"></div>
+          <div class="details">
+            <span class="title">${book.title}</span>
+            <span class="price">${book.price}</span>
+          </div>
+        </div>
+    `;
 }
